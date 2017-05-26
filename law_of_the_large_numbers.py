830a750eb3cb943939f957ad9f8af2e1afa3d779
joshuacassidy <Joshcassidy79@gmail.com>
2017-05-27 00:25:55 +0100

    Formatting the output string, and condensing the for loop that calulates each standard deviation into one line

--- law_of_the_large_numbers.py
+++ law_of_the_large_numbers.py
@@ -1,11 +1,10 @@
 import numpy as np
 from numpy.random import randn
+
 def fun(Deviations,sampleSize):
     for j in range(len(Deviations)):
-        for i in sampleSize:       
-            Deviations[j]+=1 if (-((j+1)) < i and (j+1) > i) else False
-        Deviations[j]=Deviations[j]/len(sampleSize)   
-    print(Deviations)
-    print(np.average(sampleSize))
-fun([0,0,0],randn(100000))
- 
+        #Formatting the standard deviations being looked at (68%,95% and 99.7%) into a string by using a single line generator and checking if the number is within the range of the deviation, then expressing that deviation in percentage form with two decimal spaces.    
+        Deviations[j]= "for {0} Standard deviations {1}%,".format(j+1,str(round(np.sum([Deviations[j]+1 if (-((j+1)) < i and (j+1) > i) else 0 for i in sampleSize])/len(sampleSize)*100,2)))
+
+    print("\nThe standard deviations for the sample size of {0} randomly generated normally distributed numbers are: {1}. The average of these numbers are {2}.\n".format(len(sampleSize)," ".join(Deviations),np.average(sampleSize)))
+fun([0,0,0],randn(int(input('\nWhat is the sample size of the randomly generated normally distributed numbers? \n'))))
